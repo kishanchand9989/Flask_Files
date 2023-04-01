8074b46f4ede97540211a747b2bd5a91ebcabc1e
kishanchand9989 <kishanchand9989@gmail.com>
2023-04-01 17:58:06 +0530

    this is my first push

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-
+from flask import request,redirect,url_for,render_template,jsonify
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
 
+
+@app.route("/", methods=['GET', 'POST'])
+def UserPage():
+    return render_template("Details.html")
+  
 if __name__=="__main__":
     app.run(host="0.0.0.0")
